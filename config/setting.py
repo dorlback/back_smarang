4b292ef6bf60d6073f51d232f52fb7925efb856b
백승헌 <back@baegseungheon-ui-MacBookAir.local>
2022-12-20 14:50:54 +0900

    1220

--- config/setting.py
+++ config/setting.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1','43.200.140.62','172.31.36.211','smarna
 
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -45,6 +47,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://43.200.140.62:8000",
+    "http://43.200.140.62",
+    "http://43.200.140.62:3000",
+    "http://43.200.140.62:80",
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
